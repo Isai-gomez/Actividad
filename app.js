9d2b4dacbac3726c6001c57c4d0f2565141a0939
Isai-gomez <isaigomezhernandez@gmail.com>
2020-06-17 21:06:30 -0500

    Guarda en localStorage y se muestra

--- app.js
+++ app.js
@@ -1,4 +1,41 @@
-console.log("funcionando");
 //Varialbles globales
-//Funcines
+const formularioUI = document.querySelector('#formulario');
+const listaActividadesUI  = document.querySelector('#listaActividades');
+let arrayActividades = [];
+
+//Funcines 
+const CrearItem = ( actividad ) => {
+let item = {
+  actividad: actividad,
+  estado: false
+}
+arrayActividades.push(item);
+return item
+}
+
+const GuardarDB = () => {
+  localStorage.setItem('rutina', JSON.stringify(arrayActividades));
+}
+
+const PintarDB = () => {
+  listaActividadesUI.innerHTML='';
+  arrayActividades = JSON.parse(localStorage.getItem('rutina'));
+ if(arrayActividades === null){
+    arrayActividades = [];
+ }else{
+    arrayActividades.forEach( element => {
+      listaActividades.innerHTML += `<div class="alert alert-danger" role="alert"><span class="material-icons float-left mr-2">schedule</span><b>${element.actividad}</b> <strong>${element.estado}</strong><span class="float-right"><span class="material-icons">done</span><span class="material-icons">delete_forever</span></span></div></div>`
+    });
+ }
+}
+
 //EventListener
+formularioUI.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let actividadUI = document.querySelector('#actividad').value;
+  CrearItem(actividadUI);
+  GuardarDB();
+  formularioUI.reset();
+
+});
+document.addEventListener('DOMContentLoaded', PintarDB);
