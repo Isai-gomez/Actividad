1d943e2d0d7a0b23ddc6b4ec27c388f39aece472
Isai-gomez <isaigomezhernandez@gmail.com>
2020-07-06 22:15:49 -0500

    leo nueva tarea

--- vuejsActividades/app.js
+++ vuejsActividades/app.js
@@ -1,7 +1,14 @@
 const app = new Vue({
 	el:"#app",
  	data: {
-		titulo: 'GYM con Vue'
+		titulo: 'GYM con Vue',
+	 	tareas: [],
+	        nuevaTarea:'',
+	},
+ 	methods:{
+		agregarTarea: function(){
+		console.log(this.nuevaTarea)
+	}
 	}
 
 });
