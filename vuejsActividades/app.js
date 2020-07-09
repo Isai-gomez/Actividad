6a4646f230a10f199ff9a1d463aabf860d67eee1
Isai-gomez <isaigomezhernandez@gmail.com>
2020-07-08 21:26:20 -0500

    guarda en localStarage

--- vuejsActividades/app.js
+++ vuejsActividades/app.js
@@ -12,13 +12,25 @@ const app = new Vue({
    estado:false
    });
   this.nuevaTarea="";
+  localStorage.setItem('gym-vue', JSON.stringify(this.tareas));
   },
   editarTarea: function(index){
    this.tareas[index].estado = true; 
+   localStorage.setItem('gym-vue', JSON.stringify(this.tareas));
   },
   eliminarTarea: function(index){
   this.tareas.splice(index,1);
+   localStorage.setItem('gym-vue', JSON.stringify(this.tareas));
   }
+  }, 
+ created: function(){
+   let datosDB = JSON.parse(localStorage.getItem('gym-vue'));
+  if(datosDB === null){
+    this.tareas = []
   }
+  else{
+   this.tareas = datosDB;  
+  }
+ }
 
 });
